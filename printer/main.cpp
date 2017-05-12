52869c6d7a228b301b5cbda789e1fc1f78c76322
Jeremy Greer <jeremy.greer.atl@gmail.com>
2017-05-11 20:55:40 -0500

    print a square

--- printer/main.cpp
+++ printer/main.cpp
@@ -5,11 +5,22 @@
 //  Created by Jeremy Greer on 5/11/17.
 //  Copyright © 2017 Grizzle. All rights reserved.
 //
+//  This program is going to print stuff for us to the terminal.
+//
 
 #include <iostream>
 
+void printSquare() {
+  std::cout << "+---------+" << std::endl;
+  std::cout << "|         |" << std::endl;
+  std::cout << "|         |" << std::endl;
+  std::cout << "|         |" << std::endl;
+  std::cout << "|         |" << std::endl;
+  std::cout << "|         |" << std::endl;
+  std::cout << "+---------+" << std::endl;
+}
+
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
-    return 0;
+  printSquare();
+  return 0;
 }
